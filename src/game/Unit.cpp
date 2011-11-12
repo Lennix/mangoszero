23366cd8c7280ac96f9d0bcab8668ece1ceb7613
Lennix <lennix@mc-wow.eu>
2011-11-12 23:24:07 +0100

    Combatimer for PvP is 6 sec not 5

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -6473,7 +6473,7 @@ void Unit::SetInCombatState(bool PvP, Unit* enemy)
         return;
 
     if (PvP)
-        m_CombatTimer = 5000;
+        m_CombatTimer = 6000;
 
     bool creatureNotInCombat = GetTypeId()==TYPEID_UNIT && !HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_IN_COMBAT);
 
